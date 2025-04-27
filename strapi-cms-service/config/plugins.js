69af5848591c627fa3a624c77bec5f685f242008
jeyluu <jeyluu.contact@gmail.com>
2025-04-27 13:09:43 +0700

    add docs

--- strapi-cms-service/config/plugins.js
+++ strapi-cms-service/config/plugins.js
@@ -18,4 +18,20 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'API Documentation',
+        description: 'API documentation for your Strapi project',
+      },
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+    },
+  },
 });
